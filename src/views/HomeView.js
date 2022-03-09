ebeafb4d1f93051ddaef0d0415707d0ec6eae9af
wowvaqa <wowvaqa@gmail.com>
2022-03-09 19:42:14 +0100

    ADD: Form for A & B values

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,11 +1,32 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Form } from "react-bootstrap";
 
 const HomeView = () => {
     return (
         <>
             <Container>
-                <p>Kalkulator inżynieryjny</p>
+                <h1>Kalkulator inżynieryjny</h1>
+            </Container>
+
+            <Container>
+                <Form>
+                <Form.Group
+                className="mb-3"
+                controlId="a_value"
+                
+              >
+                <Form.Label>Wartość A:</Form.Label>
+                <Form.Control type="text" />
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="b_value"
+                
+              >
+                <Form.Label>Wartość B:</Form.Label>
+                <Form.Control type="text" />
+              </Form.Group>
+                </Form>
             </Container>
         </>
     )
