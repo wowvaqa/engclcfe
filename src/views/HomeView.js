c7ebdbe18baa485ba1bdc417548c3f888b9f0c26
wowvaqa <wowvaqa@gmail.com>
2022-03-09 19:26:16 +0100

    Change: Rename Home into HomeView

---
+++ src/views/HomeView.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const HomeView = () => {
+    return (
+        <>
+            <Container>
+                <p>Kalkulator inżynieryjny</p>
+            </Container>
+        </>
+    )
+}
+
+export default HomeView
