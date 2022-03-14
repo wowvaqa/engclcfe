47eca6215ff77fca91e190b4bba0c0b3075f6484
Łukasz Wawrzyniak <wowvaqa@gamil.com>
2022-03-14 18:34:09 +0100

    Label delete

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Container, Form, Button, Label } from "react-bootstrap";
+import { Container, Form, Button } from "react-bootstrap";
 import axios from "axios";
 
 // const api = axios.create({
