9ae8976c18679b53a839faa908d3c6243071438b
Łukasz Wawrzyniak <wowvaqa@gamil.com>
2022-03-15 19:38:57 +0100

    ADD:ReinforcedConcreteCalcView Form

--- src/views/ReinforcedConcreteCalcView.js
+++ src/views/ReinforcedConcreteCalcView.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Container, Form, Button } from "react-bootstrap";
+import { Container, Form, Button, Col } from "react-bootstrap";
 import axios from "axios";
 
 const ReinforcedConcreteCalcView = () => {
@@ -28,32 +28,115 @@ const ReinforcedConcreteCalcView = () => {
   return (
     <>
       <Container>
-        <h1>Test API v. 0.0.5</h1>
+        <h1>Kalkulator żelbetu v. 0.0.1</h1>
       </Container>
 
       <Container>
         <Form>
-          <Form.Group
-            className="mb-3"
-            controlId="a_value"
-            onChange={(e) => {
-              setFirstVal(parseFloat(e.target.value.replace(",", ".")));
-            }}
-          >
-            <Form.Label>Wartość A:</Form.Label>
-            <Form.Control type="text" />
-          </Form.Group>
-          <Form.Group
-            className="mb-3"
-            controlId="b_value"
-            onChange={(e) => {
-              setSecVal(parseFloat(e.target.value.replace(",", ".")));
-            }}
-          >
-            <Form.Label>Wartość B:</Form.Label>
-            <Form.Control type="text" />
-          </Form.Group>
+          <Col>
+            <Form.Group
+              className="mb-3"
+              controlId="name_value"
+              onChange={(e) => {}}
+            >
+              <Form.Label>Nazwa:</Form.Label>
+              <Form.Control type="text" placeholder="My first cross sect" />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="b_value"
+              onChange={(e) => {
+                setSecVal(parseFloat(e.target.value.replace(",", ".")));
+              }}
+            >
+              <Form.Label>b:</Form.Label>
+              <Form.Control type="number" placeholder="0.5" />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="h_value"
+              onChange={(e) => {
+                setSecVal(parseFloat(e.target.value.replace(",", ".")));
+              }}
+            >
+              <Form.Label>h:</Form.Label>
+              <Form.Control type="number" placeholder="1.0" />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group
+              className="mb-3"
+              controlId="concrete_class"
+              onChange={(e) => {
+                setSecVal(parseFloat(e.target.value.replace(",", ".")));
+              }}
+            >
+              <Form.Label>Klasa betonu:</Form.Label>
+              <Form.Select aria-label="Default select example">
+                <option value="1">C30_37</option>
+              </Form.Select>
+            </Form.Group>
+
+            <Form.Group
+              className="mb-3"
+              controlId="steel_type"
+              onChange={(e) => {
+                setSecVal(parseFloat(e.target.value.replace(",", ".")));
+              }}
+            >
+              <Form.Label>Typ stali:</Form.Label>
+              <Form.Select aria-label="Default select example">
+                <option value="1">Bst500s</option>
+                <option value="1">B500SP</option>
+              </Form.Select>
+            </Form.Group>
+
+            <Form.Group
+              className="mb-3"
+              controlId="c_value"
+              onChange={(e) => {
+                setSecVal(parseFloat(e.target.value.replace(",", ".")));
+              }}
+            >
+              <Form.Label>c:</Form.Label>
+              <Form.Control type="number" placeholder="30" />
+            </Form.Group>
+
+            <Form.Group
+              className="mb-3"
+              controlId="fi_value"
+              onChange={(e) => {
+                setSecVal(parseFloat(e.target.value.replace(",", ".")));
+              }}
+            >
+              <Form.Label>fi:</Form.Label>
+              <Form.Control type="number" placeholder="32" />
+            </Form.Group>
+
+            <Form.Group
+              className="mb-3"
+              controlId="no_of_bar_value"
+              onChange={(e) => {
+                setSecVal(parseFloat(e.target.value.replace(",", ".")));
+              }}
+            >
+              <Form.Label>No of bar:</Form.Label>
+              <Form.Control type="number" placeholder="8" />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="fi_s_value"
+              onChange={(e) => {
+                setSecVal(parseFloat(e.target.value.replace(",", ".")));
+              }}
+            >
+              <Form.Label>fi s:</Form.Label>
+              <Form.Control type="number" placeholder="12" />
+            </Form.Group>
+          </Col>
         </Form>
+        <br></br>
+        <br></br>
         <Button
           variant="success"
           type="button"
@@ -64,9 +147,6 @@ const ReinforcedConcreteCalcView = () => {
         >
           Wyślij
         </Button>
-        <br></br>
-        <br></br>
-
         {sum === 0 && <h4>Tutaj wyświetlimy wynik działania...</h4>}
         {sum !== 0 && <h4>Wynik: {sum}</h4>}
       </Container>
