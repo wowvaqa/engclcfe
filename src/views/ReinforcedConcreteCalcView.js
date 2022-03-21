fec094c95fbf51fad457b75b37421b276a6141e4
Łukasz Wawrzyniak <wowvaqa@gamil.com>
2022-03-21 18:38:01 +0100

    ADD: Image for first calc

--- src/views/ReinforcedConcreteCalcView.js
+++ src/views/ReinforcedConcreteCalcView.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { Container, Form, Button } from "react-bootstrap";
+import { Container, Form, Button, Col, Row } from "react-bootstrap";
 import axios from "axios";
+import image from "../assets/API_1_pio.png";
 
 const ReinforcedConcreteCalcView = () => {
   /* JSON Input data */
@@ -53,127 +54,146 @@ const ReinforcedConcreteCalcView = () => {
       <Container>
         <h1>Kalkulator żelbetu v. 0.0.1</h1>
       </Container>
+      <Container fluid="md">
+        <Row>
+          <Col>
+            <Form>
+              <Form.Group
+                className="mb-3"
+                controlId="name_value"
+                onChange={(e) => {
+                  setNameValue(e.target.value);
+                }}
+              >
+                <Form.Label>Nazwa:</Form.Label>
+                <Form.Control type="text" placeholder="My first cross sect" />
+              </Form.Group>
 
-      <Container>
-        <Form>
-          <Form.Group
-            className="mb-3"
-            controlId="name_value"
-            onChange={(e) => {
-              setNameValue(e.target.value);
-            }}
-          >
-            <Form.Label>Nazwa:</Form.Label>
-            <Form.Control type="text" placeholder="My first cross sect" />
-          </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="b_value"
+                onChange={(e) => {
+                  setBValue(parseFloat(e.target.value.replace(",", ".")));
+                }}
+              >
+                <Form.Label>b:</Form.Label>
+                <Form.Control type="number" placeholder="0.5" />
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="h_value"
+                onChange={(e) => {
+                  setHValue(parseFloat(e.target.value.replace(",", ".")));
+                }}
+              >
+                <Form.Label>h:</Form.Label>
+                <Form.Control type="number" placeholder="1.0" />
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="concrete_class"
+                onChange={(e) => {
+                  setConcreteClassValue(e.target.value);
+                }}
+              >
+                <Form.Label>Klasa betonu:</Form.Label>
+                <Form.Select aria-label="Default select example">
+                  <option value="1">C30_37</option>
+                </Form.Select>
+              </Form.Group>
 
-          <Form.Group
-            className="mb-3"
-            controlId="b_value"
-            onChange={(e) => {
-              setBValue(parseFloat(e.target.value.replace(",", ".")));
-            }}
-          >
-            <Form.Label>b:</Form.Label>
-            <Form.Control type="number" placeholder="0.5" />
-          </Form.Group>
-          <Form.Group
-            className="mb-3"
-            controlId="h_value"
-            onChange={(e) => {
-              setHValue(parseFloat(e.target.value.replace(",", ".")));
-            }}
-          >
-            <Form.Label>h:</Form.Label>
-            <Form.Control type="number" placeholder="1.0" />
-          </Form.Group>
-          <Form.Group
-            className="mb-3"
-            controlId="concrete_class"
-            onChange={(e) => {
-              setConcreteClassValue(e.target.value);
-            }}
-          >
-            <Form.Label>Klasa betonu:</Form.Label>
-            <Form.Select aria-label="Default select example">
-              <option value="1">C30_37</option>
-            </Form.Select>
-          </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="steel_type"
+                onChange={(e) => {
+                  setSteelTypeValue(e.target.value);
+                }}
+              >
+                <Form.Label>Typ stali:</Form.Label>
+                <Form.Select aria-label="Default select example">
+                  <option value="1">Bst500s</option>
+                  <option value="1">B500SP</option>
+                </Form.Select>
+              </Form.Group>
 
-          <Form.Group
-            className="mb-3"
-            controlId="steel_type"
-            onChange={(e) => {
-              setSteelTypeValue(e.target.value);
-            }}
-          >
-            <Form.Label>Typ stali:</Form.Label>
-            <Form.Select aria-label="Default select example">
-              <option value="1">Bst500s</option>
-              <option value="1">B500SP</option>
-            </Form.Select>
-          </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="c_value"
+                onChange={(e) => {
+                  setCValue(parseFloat(e.target.value.replace(",", ".")));
+                }}
+              >
+                <Form.Label>c:</Form.Label>
+                <Form.Control type="number" placeholder="30" />
+              </Form.Group>
 
-          <Form.Group
-            className="mb-3"
-            controlId="c_value"
-            onChange={(e) => {
-              setCValue(parseFloat(e.target.value.replace(",", ".")));
-            }}
-          >
-            <Form.Label>c:</Form.Label>
-            <Form.Control type="number" placeholder="30" />
-          </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="fi_value"
+                onChange={(e) => {
+                  setFiValue(parseFloat(e.target.value.replace(",", ".")));
+                }}
+              >
+                <Form.Label>fi:</Form.Label>
+                <Form.Control type="number" placeholder="32" />
+              </Form.Group>
 
-          <Form.Group
-            className="mb-3"
-            controlId="fi_value"
-            onChange={(e) => {
-              setFiValue(parseFloat(e.target.value.replace(",", ".")));
+              <Form.Group
+                className="mb-3"
+                controlId="no_of_bar_value"
+                onChange={(e) => {
+                  setNoOfBarsValue(
+                    parseFloat(e.target.value.replace(",", "."))
+                  );
+                }}
+              >
+                <Form.Label>No of bar:</Form.Label>
+                <Form.Control type="number" placeholder="8" />
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="fi_s_value"
+                onChange={(e) => {
+                  setFiSValue(parseFloat(e.target.value.replace(",", ".")));
+                }}
+              >
+                <Form.Label>fi s:</Form.Label>
+                <Form.Control type="number" placeholder="12" />
+              </Form.Group>
+            </Form>
+          </Col>
+          <Col>
+            <Container>
+              <img
+                src={image}
+                width={732}
+                height={800}
+                alt="cocktail db logo"
+                className="weightElement"
+              />
+            </Container>
+          </Col>
+        </Row>
+        <Row>
+          <Button
+            variant="success"
+            type="button"
+            className="btn btn-primary"
+            onClick={(event) => {
+              sendData(event);
             }}
           >
-            <Form.Label>fi:</Form.Label>
-            <Form.Control type="number" placeholder="32" />
-          </Form.Group>
-
-          <Form.Group
-            className="mb-3"
-            controlId="no_of_bar_value"
-            onChange={(e) => {
-              setNoOfBarsValue(parseFloat(e.target.value.replace(",", ".")));
-            }}
-          >
-            <Form.Label>No of bar:</Form.Label>
-            <Form.Control type="number" placeholder="8" />
-          </Form.Group>
-          <Form.Group
-            className="mb-3"
-            controlId="fi_s_value"
-            onChange={(e) => {
-              setFiSValue(parseFloat(e.target.value.replace(",", ".")));
-            }}
-          >
-            <Form.Label>fi s:</Form.Label>
-            <Form.Control type="number" placeholder="12" />
-          </Form.Group>
-        </Form>
-
-        <Button
-          variant="success"
-          type="button"
-          className="btn btn-primary"
-          onClick={(event) => {
-            sendData(event);
-          }}
-        >
-          Wyślij
-        </Button>
-        {m_rd === 0 && <h4>Tutaj wyświetlimy wynik...</h4>}
-        {m_rd !== 0 && (
-          <h4>
-            m_rd: {m_rd} ksi_eff: {ksi_eff} x_eff: {x_eff}
-          </h4>
-        )}
+            Wyślij
+          </Button>
+        </Row>
+        <Row>
+          {m_rd === 0 && <h4>Tutaj wyświetlimy wynik...</h4>}
+          {m_rd !== 0 && (
+            <h4>
+              m_rd: {m_rd} ksi_eff: {ksi_eff} x_eff: {x_eff}
+            </h4>
+          )}
+        </Row>
       </Container>
     </>
   );
