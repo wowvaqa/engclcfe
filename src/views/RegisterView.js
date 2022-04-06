50402da02e5be35380d80e7a3b75e165942d827b
Łukasz Wawrzyniak <wowvaqa@gamil.com>
2022-04-06 18:55:36 +0200

    REF: Registration view

--- src/views/RegisterView.js
+++ src/views/RegisterView.js
@@ -1,6 +1,6 @@
 import { useRef, useState, useEffect } from "react";
 import axios from "axios";
-import { Button, Container, Form } from "react-bootstrap";
+import { Button, Container, Form, Row, Col } from "react-bootstrap";
 
 const EMAIL_REGEX = /^(?=.*[a-z])(?=.*[@]).{8,50}$/;
 const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
@@ -112,124 +112,139 @@ const RegisterView = () => {
               {errMsg}
             </p>
             <h1>Register</h1>
-
             <Form>
-              <Form.Group
-                onSubmit={handleSubmit}
-                className="mb-3"
-                controlId="email"
-              >
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                  type="email"
-                  ref={emailRef}
-                  autoComplete="off"
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
-                  aria-invalid={validEmail ? "false" : "true"}
-                  aria-describedby="uidnote"
-                  onFocus={() => setEmailFocus(true)}
-                  onBlur={() => setEmailFocus(false)}
-                />
-                <p
-                  id="uidnote"
-                  className={
-                    emailFocus && !validEmail ? "instructions" : "offscreen"
-                  }
-                >
-                  8 to 50 characters.
-                  <br />@ required
-                </p>
-              </Form.Group>
-              <Form.Group
-                onSubmit={handleSubmit}
-                className="mb-3"
-                controlId="username"
-              >
-                <Form.Label>Username</Form.Label>
-                <Form.Control
-                  type="text"
-                  autoComplete="off"
-                  onChange={(e) => setUser(e.target.value)}
-                  value={user}
-                  required
-                  aria-invalid={validName ? "false" : "true"}
-                  aria-describedby="uidnote"
-                  onFocus={() => setUserFocus(true)}
-                  onBlur={() => setUserFocus(false)}
-                />
-              </Form.Group>
-              <p
-                id="uidnote"
-                className={
-                  userFocus && user && !validName ? "instructions" : "offscreen"
-                }
-              >
-                4 to 24 characters.
-                <br />
-                Must begin with a letter.
-                <br />
-                Letters, numbers, underscores, hyphens allowed.
-              </p>
-              <Form.Group
-                onSubmit={handleSubmit}
-                className="mb-3"
-                controlId="password"
-              >
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                  type="password"
-                  onChange={(e) => setPwd(e.target.value)}
-                  value={pwd}
-                  required
-                  aria-invalid={validPwd ? "false" : "true"}
-                  aria-describedby="pwdnote"
-                  onFocus={() => setPwdFocus(true)}
-                  onBlur={() => setPwdFocus(false)}
-                />
-              </Form.Group>
-              <p
-                id="pwdnote"
-                className={pwdFocus && !validPwd ? "instructions" : "offscreen"}
-              >
-                8 to 24 characters.
-                <br />
-                Must include uppercase and lowercase letters, a number and a
-                special character.
-                <br />
-                Allowed special characters:{" "}
-                <span aria-label="exclamation mark">!</span>{" "}
-                <span aria-label="at symbol">@</span>{" "}
-                <span aria-label="hashtag">#</span>{" "}
-                <span aria-label="dollar sign">$</span>{" "}
-                <span aria-label="percent">%</span>
-              </p>
+              <Row>
+                <Col>
+                  <Form.Group
+                    onSubmit={handleSubmit}
+                    className="mb-3"
+                    controlId="email"
+                  >
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control
+                      type="email"
+                      ref={emailRef}
+                      autoComplete="off"
+                      onChange={(e) => setEmail(e.target.value)}
+                      required
+                      aria-invalid={validEmail ? "false" : "true"}
+                      aria-describedby="uidnote"
+                      onFocus={() => setEmailFocus(true)}
+                      onBlur={() => setEmailFocus(false)}
+                    />
+                    <p
+                      id="uidnote"
+                      className={
+                        emailFocus && !validEmail ? "instructions" : "offscreen"
+                      }
+                    >
+                      8 to 50 characters.
+                      <br />@ required
+                    </p>
+                  </Form.Group>
+                </Col>
+                <Col>
+                  <Form.Group
+                    onSubmit={handleSubmit}
+                    className="mb-3"
+                    controlId="username"
+                  >
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control
+                      type="text"
+                      autoComplete="off"
+                      onChange={(e) => setUser(e.target.value)}
+                      value={user}
+                      required
+                      aria-invalid={validName ? "false" : "true"}
+                      aria-describedby="uidnote"
+                      onFocus={() => setUserFocus(true)}
+                      onBlur={() => setUserFocus(false)}
+                    />
+                  </Form.Group>
+                  <p
+                    id="uidnote"
+                    className={
+                      userFocus && user && !validName
+                        ? "instructions"
+                        : "offscreen"
+                    }
+                  >
+                    4 to 24 characters.
+                    <br />
+                    Must begin with a letter.
+                    <br />
+                    Letters, numbers, underscores, hyphens allowed.
+                  </p>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <Form.Group
+                    onSubmit={handleSubmit}
+                    className="mb-3"
+                    controlId="password"
+                  >
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                      type="password"
+                      onChange={(e) => setPwd(e.target.value)}
+                      value={pwd}
+                      required
+                      aria-invalid={validPwd ? "false" : "true"}
+                      aria-describedby="pwdnote"
+                      onFocus={() => setPwdFocus(true)}
+                      onBlur={() => setPwdFocus(false)}
+                    />
+                  </Form.Group>
+                  <p
+                    id="pwdnote"
+                    className={
+                      pwdFocus && !validPwd ? "instructions" : "offscreen"
+                    }
+                  >
+                    8 to 24 characters.
+                    <br />
+                    Must include uppercase and lowercase letters, a number and a
+                    special character.
+                    <br />
+                    Allowed special characters:{" "}
+                    <span aria-label="exclamation mark">!</span>{" "}
+                    <span aria-label="at symbol">@</span>{" "}
+                    <span aria-label="hashtag">#</span>{" "}
+                    <span aria-label="dollar sign">$</span>{" "}
+                    <span aria-label="percent">%</span>
+                  </p>
+                </Col>
+                <Col>
+                  <Form.Group
+                    onSubmit={handleSubmit}
+                    className="mb-3"
+                    controlId="confirm_pwd"
+                  >
+                    <Form.Label>Confirm Password:</Form.Label>
+                    <Form.Control
+                      type="password"
+                      onChange={(e) => setMatchPwd(e.target.value)}
+                      value={matchPwd}
+                      required
+                      aria-invalid={validMatch ? "false" : "true"}
+                      aria-describedby="confirmnote"
+                      onFocus={() => setMatchFocus(true)}
+                      onBlur={() => setMatchFocus(false)}
+                    />
+                  </Form.Group>
+                  <p
+                    id="confirmnote"
+                    className={
+                      matchFocus && !validMatch ? "instructions" : "offscreen"
+                    }
+                  >
+                    Must match the first password input field.
+                  </p>
+                </Col>
+              </Row>
 
-              <Form.Group
-                onSubmit={handleSubmit}
-                className="mb-3"
-                controlId="confirm_pwd"
-              >
-                <Form.Label>Confirm Password:</Form.Label>
-                <Form.Control
-                  type="password"
-                  onChange={(e) => setMatchPwd(e.target.value)}
-                  value={matchPwd}
-                  required
-                  aria-invalid={validMatch ? "false" : "true"}
-                  aria-describedby="confirmnote"
-                  onFocus={() => setMatchFocus(true)}
-                  onBlur={() => setMatchFocus(false)}
-                />
-              </Form.Group>
-              <p
-                id="confirmnote"
-                className={
-                  matchFocus && !validMatch ? "instructions" : "offscreen"
-                }
-              >
-                Must match the first password input field.
-              </p>
               <Button
                 disabled={!validName || !validPwd || !validMatch ? true : false}
                 variant="primary"
