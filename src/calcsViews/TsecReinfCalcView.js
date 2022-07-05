6004e8847593f6dbce695da5288961a57de42e11
Łukasz Wawrzyniak <wowvaqa@gmail.com>
2022-07-05 23:22:33 +0200

    FIX: imports

--- src/calcsViews/TsecReinfCalcView.js
+++ src/calcsViews/TsecReinfCalcView.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Row, Form, Col, Button } from "react-bootstrap";
 import image from "../assets/API_3_pio.png";
 
